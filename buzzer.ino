bf40c5265c1d9ab12ede9f61aeca13ed8485554a
Selvabharathi S <40407517+selvabharathis@users.noreply.github.com>
2018-08-02 22:02:55 +0530

    Add files via upload

---
+++ buzzer.ino
@@ -0,0 +1,15 @@
+//S.Selvabharathi  B.Tech IT  Rajalakshmi Engineering College
+// code to ping the buzzer
+const int buz=13;
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(buz,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(buz,HIGH);
+}
+
