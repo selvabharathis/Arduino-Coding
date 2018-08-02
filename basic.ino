1d89c0568ce7246a5b8150548c7f9def8dbd22e1
Selvabharathi S <40407517+selvabharathis@users.noreply.github.com>
2018-08-02 21:59:31 +0530

    Add files via upload

---
+++ basic.ino
@@ -0,0 +1,18 @@
+// S.Selvabharathi B.Tech  IT  Rajalakshmi Engineering College
+//this is a sample code to take any device to work with microcontroller,such as to run and stop a motor,fan,led,ldr etc.
+const int fanpin=16;
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(115200);
+  delay(10);
+  pinMode(fanpin,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+//delay(100000000);
+// digitalWrite(fanpin,LOW);
+// delay(100000000);
+  digitalWrite(fanpin,HIGH);
+}
+
