0e1e05551363274a01809f02b005225fdc4c28ab
Selvabharathi S <40407517+selvabharathis@users.noreply.github.com>
2018-08-20 02:05:10 +0530

    Add files via upload

---
+++ pir_sensor-led/pir_sensor-led.ino
@@ -0,0 +1,27 @@
+//   S.Selvabharathi  B.Tech IT   Rajalakshmi Engineering College 
+// vcc->3v to 5v , gnd->gnd ,out->analog pin of microcontroller
+//NOTE: pir has sensitivity adjustment,duration adjestment and trigger(non-repeating and repeating) if trigger is in repeat mode the led will blink on and off , so becarefull with all the constrains.
+
+int led = D6;
+int pir=D7;
+int state=0;
+
+void setup() {
+  pinMode(pir, INPUT); 
+  pinMode(led, OUTPUT); 
+}
+
+void loop(){
+  state = digitalRead(pir);
+  delay(1000);
+    if(state == HIGH)
+    {
+      digitalWrite (led, HIGH);
+      
+      }
+    if(state == LOW)
+    {
+      digitalWrite (led, LOW);
+    }
+}
+
