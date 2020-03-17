7035b8d733176f12e7b8b6b038b2adffc700aedf
Selvabharathi S <selvabharathi.s.2016.it@rajalakshmi.edu.in>
2020-03-17 18:09:49 +0530

    Add files via upload

---
+++ two way nrf communication/NRFtwo.ino
@@ -0,0 +1,27 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+RF24 radio(7, 8); // CE, CSN
+const byte addresses[][6] = {"00001", "00002"};
+void setup() {
+  Serial.begin(9600);
+  radio.begin();
+  radio.openWritingPipe(addresses[0]); // 00001
+  radio.openReadingPipe(1, addresses[1]); // 00002
+  radio.setPALevel(RF24_PA_MIN);
+}
+void loop() 
+{
+  delay(5);
+  radio.startListening();
+  if ( radio.available()) 
+  {
+      char textReceive[32];
+      radio.read(&textReceive, sizeof(textReceive));
+      Serial.println(textReceive);
+   }
+   delay(5);
+   radio.stopListening();
+   char textSend[] ="TransmitTWO";
+   radio.write(&textSend, sizeof(textSend));
+}
